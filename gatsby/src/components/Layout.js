933b8a21c6013f8c2e257ff87f70b798a62da183
Will Luft <william.d.luft@gmail.com>
2020-10-25 11:13:20 -0400

    Added background image for home page

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -4,19 +4,30 @@ import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Nav from './Nav';
 import Footer from './Footer';
+import battlestation from '../assets/images/battlestation.jpg';
 
 const LayoutWrapper = styled.div`
   main {
     max-width: 48rem;
     margin: 0 auto;
-    padding: 2rem 1rem;
+    padding: 2rem 2rem;
+  }
+
+  &.welcome {
+    background: url(${battlestation}) no-repeat top center;
+  }
+
+  @media screen and (min-width: 55rem) {
+    main {
+      padding: 2rem 0;
+    }
   }
 `;
 
-const Layout = ({ children }) => (
+const Layout = ({ children, location }) => (
   <>
     <GlobalStyles />
-    <LayoutWrapper>
+    <LayoutWrapper className={location.pathname === '/' ? 'welcome' : null}>
       <Nav />
       <main>{children}</main>
       <Footer />
