d177123c5a064c0f5d7fedb92ff38f82d680bea4
Will Luft <william.d.luft@gmail.com>
2020-10-28 17:57:58 -0400

    Styled and created project page

---
+++ gatsby/src/utils/projects.js
@@ -0,0 +1,29 @@
+export const projectsList = [
+  {
+    title: 'ManaMaker',
+    url: 'https://manamaker.site/',
+    github: 'https://github.com/wdluft/manamaker-version-2',
+    technologies: ['React', 'Styled Components', 'Framer Motion'],
+    description:
+      'A simple calculator to help figure out a mana base for Magic the Gathering.',
+    image: 'manamaker',
+  },
+  {
+    title: 'Random Commander',
+    url: 'https://randomcommander.com/',
+    github: 'https://github.com/wdluft/randomcommander',
+    technologies: ['HTML', 'SASS', 'JavaScript'],
+    description:
+      'Selects a random card that can be a commander for a Magic the Gathering EDH deck.',
+    image: 'randomcommander',
+  },
+  {
+    title: 'Tomato Timer',
+    url: 'https://mystifying-heisenberg-8bc659.netlify.com/',
+    github: 'https://github.com/wdluft/tomato-timer',
+    technologies: ['React', 'Styled Components'],
+    description:
+      'Pomodoro timer to help schedule work/break sessions for increased productivity.',
+    image: 'tomatotimer',
+  },
+];
