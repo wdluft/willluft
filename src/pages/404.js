f77f42ddbbb80dbfb6f0ace258b7cbf5424d463f
Will Luft <william.d.luft@gmail.com>
2021-03-11 18:30:43 -0500

    Added links to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,41 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+import SEO from '../components/SEO';
+
+const FourOhFourWrapper = styled.div`
+  min-height: 75vh;
+
+  a {
+    font-size: var(--h5);
+  }
+`;
 
 const FourOhFourPage = () => (
-  <div>
-    <h2>This page doesn't exist.</h2>
-  </div>
+  <>
+    <SEO title="404: Page not found" />
+    <FourOhFourWrapper>
+      <h2>404: Page not found</h2>
+      <p>
+        The page you're looking for doesn't exist. You might be looking for one
+        of these pages:
+      </p>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/bookshelf">Bookshelf</Link>
+        </li>
+      </ul>
+    </FourOhFourWrapper>
+  </>
 );
 
 export default FourOhFourPage;
