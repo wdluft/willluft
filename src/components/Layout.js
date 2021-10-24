ad9d2f27bf990d9e3c5cf2e633ba664ae9415c32
Will Luft <william.d.luft@gmail.com>
2021-10-24 17:45:00 -0400

    Removed unused imports

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import 'normalize.css';
 import '@fontsource/rubik';
-import '@fontsource/rubik/variable.css';
 import '@fontsource/fira-mono';
 import GlobalStyles from '../styles/GlobalStyles';
 import Header from './Header';
