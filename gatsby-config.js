7e21a55ed7d59bb671e2624eb475b5d66154daeb
Will Luft <william.d.luft@gmail.com>
2021-11-14 13:24:57 -0500

    Adding stuff to deploy to netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-netlify',
     'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
