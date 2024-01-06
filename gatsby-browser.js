6aa42c561042db0445c817077f875e23e7391228
Will Luft <william.d.luft@gmail.com>
2024-01-06 11:35:21 -0500

    Removed all files to start from scratch

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from './src/components/Layout';
-import '@fontsource/rubik/variable.css';
-import '@fontsource/fira-mono';
-
-export function wrapPageElement({ element, props }) {
-  return <Layout {...props}>{element}</Layout>;
-}
--- src/components/ComingSoon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ComingSoon = () => (
-  <h3>I'm still working on this page, it should be done soon (TM)</h3>
-);
-
-export default ComingSoon;
