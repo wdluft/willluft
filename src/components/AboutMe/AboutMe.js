3fb2d2a7fea78106968e78476e4788eae6e798f1
Will Luft <william.d.luft@gmail.com>
2024-03-21 13:40:39 -0400

    Added new-component package and updated file/folder structure

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutMe = () => {
+  return <ul>
+    <li>
+      Information and Business Technology
+    </li>
+    <li>Leatherwork</li>
+    <li>Soccer</li>
+    <li>Volleyball</li>
+  </ul>;
+};
+
+export default AboutMe;
---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AboutMe from "../AboutMe/AboutMe";
+
+
+
+const  App = () => {
+  return (
+    <div className="App">
+      <h1>Will Luft</h1>
+      <AboutMe />
+    </div>
+  );
+}
+
+export default App;
